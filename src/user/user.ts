2994062a502b0d835ee52529d66a77e139e48551
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-11-07 16:36:31 +0100

    initial channel api and functions

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,20 +1,80 @@
 import type { Message } from "../message/message.js"
-import type { Channel } from "../channel/channel.js"
+import { Channel } from "../channel/channel.js"
+import { f } from "vitest/dist/index-40e0cb97.js";
 
 //User identified by ID in code, by NAME + DUPLICATEID(=4 numbers) by user
 export class User{
-    UID: id;
-    NAME: string;
-    DUPLICATEID: number; //kind of like discord for identifying people with same name
-    password: string;
-    channels: Channel[];
-    connectedChannel: Channel; //what if haven't joined channel? Perhaps default channel?
-    timeConnectedChannel: Date;
-    timeConnectedServer: Date;
-    DATECREATED: Date;
+    private UID: string;
+    private name: string;
+    //DUPLICATEID: number; //kind of like discord for identifying people with same name
+    private password: string;
+    private channels: Set<string>;
+    private friends: Set<string>;
+    private connectedChannel: Channel | undefined; //what if haven't joined channel? Perhaps default channel?
+    private timeConnectedChannel: number;
+    private timeConnectedServer: number;
+    private DATECREATED: number;
     
-    constructor(UID:id, name: string, duplicateid:number, password: string, channels: Channel[], datecreated: Date){
-        
+    constructor(name: string, password: string){
+        this.UID = Date.now().toString(36);
+        this.name = name;
+        this.password = password;
+        this.channels = new Set<string>;
+        this.friends = new Set<string>;
+        this.connectedChannel = undefined;
+        this.timeConnectedChannel = 0;
+        this.timeConnectedServer = 0;
+        this.DATECREATED = Date.now()
+    }
+
+    addFriend(friend: User){
+        if(this.friends.has(friend.getUID())){ 
+            console.log("ERROR users are already friends");
+            return;
+        }
+        this.friends.add(friend.getUID());
+        friend.addFriend(this);
+    }
+
+    removeFriend(friend: User){
+        if(!this.friends.has(friend.getUID())){
+            console.log("ERROR users are not friends");
+            return;
+        }
+        this.friends.delete(friend.getUID())
+        friend.removeFriend(this);
+    }
+
+    isFriend(friend: User){
+        return this.friends.has(friend.getUID())
+    }
+
+    getUID(){
+        return this.UID;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getPassword(){
+        return this.password;
+    }
+
+    getFriends(){
+        return this.friends.values
+    }
+
+    getChannels(){
+        return this.channels.values;
+    }
+
+    getConnectedChannel(){
+        return this.getConnectedChannel;
+    }
+
+    isConnected(){
+        return this.timeConnectedServer != 0;
     }
  }
  function userJoin(name: string, password: string){} // goes to constructor if can't find user
