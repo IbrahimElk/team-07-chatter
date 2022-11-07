153a17bca5231e4936bd91bc702e1ee04236e70d
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-11-07 15:07:53 +0100

    testing savong of channels

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -1,16 +1,22 @@
+import { d } from "vitest/dist/index-40e0cb97.js";
 import type { Message } from "../message/message.js"
 import type { User } from "../user/user.js";
 
 //Channel identified by ID in code, by NAME + DUPLICATEID(=4 numbers) by user
 export class Channel{
-    CID: id;
-    NAME: string;
-    DUPLICATEID: number; //get around same name
+    // CID: id;
+    public NAME: string;
+    DUPLICATEID: number = 0; //get around same name
     messages: Message[];
     users: User[];
-    connected: User[];
-    DATECREATED: Date;
+    connected: User[] = [];
+    DATECREATED: number;
     
-    constructor(cid:id, name: string, messages: Message[], users:User[], datecreated: number){}
+    constructor(name: string, messages: Message[], users:User[], datecreated: number){
+        this.NAME = name;
+        this.messages = messages;
+        this.users = users;
+        this.DATECREATED = datecreated;
+        return this
+    }
  }
- function deleteChannel(channel: Channel){}
