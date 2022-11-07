4a9ca3e2345330e76c991bbba064b309960c3460
Guust Luyckx <guust.luyckx@student.kuleuven.be>
2022-11-07 16:12:54 +0100

    test of file

--- src/database/json_generator.ts
+++ src/database/json_generator.ts
@@ -2,13 +2,14 @@
 //Date: 2022/10/31
 
 import fs from 'fs';
-
+import { Channel } from '../channel/channel.js';
+import type { User } from '../user/user.js';
 
 function channelSave(channel: Channel |  Channel[]) {
     if (channel instanceof Array)  {
         for (const x of channel) {
             const obj = JSON.stringify(x);
-            let id = x.id.tostring();
+            let id = x.NAME.toString();
             let path = '../assets/database/channels/' + id + '.json';
             fs.writeFile(path, obj, err => {
                 if (err) {
@@ -20,7 +21,7 @@ function channelSave(channel: Channel |  Channel[]) {
     }
     else {
         const obj = JSON.stringify(channel);
-        let id = channel.id.tostring();
+        let id = channel.NAME.toString();
         let path = '../assets/database/channels/' + id + '.json';
         fs.writeFile(path, obj, err => {
             if (err) {
@@ -31,59 +32,61 @@ function channelSave(channel: Channel |  Channel[]) {
     }
 }
 
-function channelsLoad() {
-    const directory = fs.opendirSync('../assets/database/channels');
-    let file;
-    while ((file = directory.readSync()) !== null) {
-        fs.readFile(file.name, 'utf-8', (err, data) => {
-            if (err) {
-                throw err
-            }
-            console.log('JSON data is saved.')
-            const parsedobj = JSON.parse(data.toString());
-            //add channel to server list
-        })
-    }
-    directory.closeSync();
-}
+channelSave(new Channel('Barteld', [], [], 0));
+
+// function channelsLoad() {
+//     const directory = fs.opendirSync('../assets/database/channels');
+//     let file;
+//     while ((file = directory.readSync()) !== null) {
+//         fs.readFile(file.name, 'utf-8', (err, data) => {
+//             if (err) {
+//                 throw err
+//             }
+//             console.log('JSON data is saved.')
+//             const parsedobj = JSON.parse(data.toString());
+//             //add channel to server list
+//         })
+//     }
+//     directory.closeSync();
+// }
 
 
-function userSave(user: User) {
-    const obj = JSON.stringify(user);
-    let name = user.name.tostring();
-    let path = '../assets/database/users/' + name + '.json';
-    fs.writeFile(path, obj, err => {
-        if (err) {
-            throw err
-        }
-        console.log('JSON data is saved.')
-    })
-}
+// function userSave(user: User) {
+//     const obj = JSON.stringify(user);
+//     let name = user.name.tostring();
+//     let path = '../assets/database/users/' + name + '.json';
+//     fs.writeFile(path, obj, err => {
+//         if (err) {
+//             throw err
+//         }
+//         console.log('JSON data is saved.')
+//     })
+// }
 
-function userLoad(username: string) {
-    let path = '../assets/database/users/' + username + '.json';
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-            throw err
-        }
-        console.log('JSON data is saved.')
-        const parsedobj = JSON.parse(data.toString());
-            //add user to server list
-    })
-}
+// function userLoad(username: string) {
+//     let path = '../assets/database/users/' + username + '.json';
+//     fs.readFile(path, 'utf-8', (err, data) => {
+//         if (err) {
+//             throw err
+//         }
+//         console.log('JSON data is saved.')
+//         const parsedobj = JSON.parse(data.toString());
+//             //add user to server list
+//     })
+// }
 
-function usersLoad() {
-    const directory = fs.opendirSync('../assets/database/users');
-    let file;
-    while ((file = directory.readSync()) !== null) {
-        fs.readFile(file.name, 'utf-8', (err, data) => {
-            if (err) {
-                throw err
-            }
-            console.log('JSON data is saved.')
-            const parsedobj = JSON.parse(data.toString());
-            //add user to server list
-        })
-    }
-    directory.closeSync();
-}
+// function usersLoad() {
+//     const directory = fs.opendirSync('../assets/database/users');
+//     let file;
+//     while ((file = directory.readSync()) !== null) {
+//         fs.readFile(file.name, 'utf-8', (err, data) => {
+//             if (err) {
+//                 throw err
+//             }
+//             console.log('JSON data is saved.')
+//             const parsedobj = JSON.parse(data.toString());
+//             //add user to server list
+//         })
+//     }
+//     directory.closeSync();
+// }
