3a231ab20eb5711c33fd55c2c4a1b0f293cbd80d
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-11-14 05:29:08 +0100

    Remove channel field too

--- src/message/message.ts
+++ src/message/message.ts
@@ -4,12 +4,10 @@ import { server } from "../server/server.js"
 export class Message{
     DATE: Date;
     USER: User;
-    CHANNEL: Channel;
     TEXT: string;
     
     constructor(user: User, channel: Channel, text: string){
         this.USER = user;
-        this.CHANNEL = channel;
         this.TEXT = text;
         this.DATE = new Date(); // assign new data
     }
