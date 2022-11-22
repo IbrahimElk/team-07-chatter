b48ab8aa1bc757ed9dfc3ea5a7ad1f5a82d14c2b
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-11-22 20:45:16 +0100

    changed tojson on channels to return UUID instead of users

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -6,6 +6,7 @@ import type { User } from '../user/user.js';
 import type { UUID } from '../user/uuid.js';
 import { server } from '../server/server.js';
 import { CUID } from './cuid.js';
+import type { MockedObject } from 'vitest';
 
 export abstract class Channel {
   private readonly CUID;
