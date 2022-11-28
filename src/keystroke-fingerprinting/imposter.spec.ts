b3df9d302ec7539cd563afe7f019b03b4c3ec65c
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2022-11-28 15:11:22 +0100

    first commit

---
+++ src/keystroke-fingerprinting/imposter.spec.ts
@@ -0,0 +1,66 @@
+// @author thomasevenepoel
+// @date 2022-11-27
+
+import { describe, it, expect } from 'vitest';
+import { CompareTwoMaps } from './compare-two-maps.js';
+import { imposter } from './imposter.js';
+import { aMeasure, rMeasure } from './measures.js';
+
+describe('Checks for an imposter', () => {
+  it('Compares two maps and indicates that the map is an imposter or not.', () => {
+    expect(
+      imposter(
+        new Map<string, number>([
+          ['he', 90],
+          ['el', 50],
+          ['ll', 40],
+          ['lo', 60],
+        ]),
+        new Map<string, number>([
+          ['he', 30],
+          ['el', 60],
+          ['ll', 90],
+          ['lo', 20],
+        ]),
+        1,
+        0.25,
+        0.75
+      )
+    ).toEqual(true); // TOTAL IS EQUAL TO 1
+
+    expect(
+      imposter(
+        new Map<string, number>([
+          ['he', 150],
+          ['el', 170],
+        ]),
+        new Map<string, number>([
+          ['he', 90],
+          ['el', 50],
+          ['ll', 40],
+          ['lo', 60],
+        ]),
+        1,
+        0.25,
+        0.75
+      )
+    );
+    expect(
+      imposter(
+        new Map<string, number>([
+          ['he', 90],
+          ['el', 50],
+          ['ll', 40],
+        ]),
+        new Map<string, number>([
+          ['he', 90],
+          ['el', 50],
+          ['ll', 80],
+        ]),
+        1,
+        0.25,
+        0.75
+      )
+    );
+  });
+});
---
+++ src/keystroke-fingerprinting/imposter.ts
@@ -0,0 +1,32 @@
+// @author thomasevenepoel
+// @date 2022-11-27
+
+import { CompareTwoMaps } from './compare-two-maps.js';
+import { aMeasure, rMeasure } from './measures.js';
+
+/**
+ * A fucnction that checks if a certain map of keystrokes is from the right person
+ * @param map1 A map containing the keystrokes that are stored in the database
+ * @param map2 A map containing the keystrokes that are sent
+ * @returns A boolean indicating that an map of N-grams is an imposter or not.
+ * @author thomasevenepoel
+ */
+export function imposter(
+  map1: Map<string, number>,
+  map2: Map<string, number>,
+  treshold: number,
+  aPercentage: number,
+  rPercentage: number
+): boolean {
+  const a = aMeasure(map1, map2);
+
+  const ordering_vector = CompareTwoMaps(map1, map2);
+
+  const r = rMeasure(ordering_vector);
+
+  if (aPercentage * a + rPercentage * r <= treshold) {
+    return true;
+  } else {
+    return false;
+  }
+}
