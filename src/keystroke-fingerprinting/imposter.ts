f58b0ab6e83fa2edd733e00cdbfcdddbeea836fd
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2022-11-28 15:57:21 +0100

    normalized a-measure

--- src/keystroke-fingerprinting/imposter.ts
+++ src/keystroke-fingerprinting/imposter.ts
@@ -5,7 +5,7 @@ import { CompareTwoMaps } from './compare-two-maps.js';
 import { aMeasure, rMeasure } from './measures.js';
 
 /**
- * A fucnction that checks if a certain map of keystrokes is from the right person
+ * A function that checks if a certain map of keystrokes is from the right person
  * @param map1 A map containing the keystrokes that are stored in the database
  * @param map2 A map containing the keystrokes that are sent
  * @returns A boolean indicating that an map of N-grams is an imposter or not.
@@ -24,7 +24,8 @@ export function Detective(
 
   const r = rMeasure(ordering_vector);
 
-  if (aPercentage * a + rPercentage * r <= treshold) {
+  const normalized_a = Math.atan(a);
+  if (aPercentage * normalized_a + rPercentage * r <= treshold) {
     return true;
   } else {
     return false;
