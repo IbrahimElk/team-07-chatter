47eee34279361181fcfe30828d0dc9c260061ba7
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-11-29 04:56:00 +0100

    fixed channels to work without saving objects (as we already know this works)

--- src/channel/channel.spec.ts
+++ src/channel/channel.spec.ts
@@ -8,28 +8,25 @@ import { Message } from '../message/message.js';
 import { serverInstance, serverSave } from '../database/server_database.js';
 
 /**
- * Tests basic functionalities of channel object. Only to be tested on fresh servers.
- * Does not work on gitlab due to adding files.
+ * Tests basic functionalities of channel object.
  */
 describe('Channel', () => {
-  it('name tests', async () => {
-    // const user = new User('owner', 'powner');
-    // const channel = new PublicChannel('publicChannel', user);
-    // expect(channel.getName()).toEqual('publicChannel');
-    // expect(channel.getUsers().size).toEqual(1);
-    // expect(channel.getOwner()).toEqual(user);
-    // const user2 = new User('Heeey', 'Hooo');
-    // channel.addUser(user2);
-    // expect(channel.getUsers().size).toEqual(2);
-    // const firstMessage = new Message(user, 'First message');
-    // channel.addMessage(firstMessage);
-    // expect(channel.getMessages().length).toEqual(1);
-    // const secondMessage = new Message(user2, 'First message');
-    // channel.addMessage(secondMessage);
-    // expect(channel.getMessages().length).toEqual(2);
-    // expect(channel.getMessages(2)[0]).toEqual(secondMessage);
-    // expect(channel.getMessages(2)[1]).toEqual(firstMessage);
-    // console.log(serverInstance);
-    // await serverSave(serverInstance, 'test');
+  it('name tests', () => {
+    const user = new User('owner', 'powner');
+    const channel = new PublicChannel('publicChannel', user);
+    expect(channel.getName()).toEqual('publicChannel');
+    expect(channel.getUsers().size).toEqual(1);
+    expect(channel.getOwner()).toEqual(user);
+    const user2 = new User('Heeey', 'Hooo');
+    channel.addUser(user2);
+    expect(channel.getUsers().size).toEqual(2);
+    const firstMessage = new Message(user, 'First message');
+    channel.addMessage(firstMessage);
+    expect(channel.getMessages().length).toEqual(1);
+    const secondMessage = new Message(user2, 'First message');
+    channel.addMessage(secondMessage);
+    expect(channel.getMessages().length).toEqual(2);
+    expect(channel.getMessages(2)[0]).toEqual(secondMessage);
+    expect(channel.getMessages(2)[1]).toEqual(firstMessage);
   });
 });
--- src/user/user.spec.ts
+++ src/user/user.spec.ts
@@ -6,27 +6,24 @@ import { serverInstance, serverSave } from '../database/server_database.js';
 import { User } from '../user/user.js';
 
 /**
- * Tests basic functionalities of User object. Only to be tested on fresh server.
- * Does not work on gitlab due to adding files.
+ * Tests basic functionalities of User object.
  */
 describe('User', () => {
-  it('password and name tests', async () => {
-    // const user = new User('Hello', 'world');
-    // expect(user.getName()).toEqual('Hello');
-    // expect(user.getPassword()).toEqual('world');
-    // user.setName('newName');
-    // user.setPassword('newPassword');
-    // expect(user.getName()).toEqual('newName');
-    // expect(user.getPassword()).toEqual('newPassword');
-    // await serverSave(serverInstance, 'test');
+  it('password and name tests', () => {
+    const user = new User('Hello', 'world');
+    expect(user.getName()).toEqual('Hello');
+    expect(user.getPassword()).toEqual('world');
+    user.setName('newName');
+    user.setPassword('newPassword');
+    expect(user.getName()).toEqual('newName');
+    expect(user.getPassword()).toEqual('newPassword');
   });
-  it('friend test', async () => {
-    // const user = new User('Hello', 'world');
-    // const friend = new User('Goodbye', 'world');
-    // expect(user.isFriend(friend)).toEqual(false);
-    // user.addFriend(friend);
-    // expect(user.isFriend(friend)).toEqual(true);
-    // expect(friend.isFriend(user)).toEqual(true);
-    // await serverSave(serverInstance, 'test');
+  it('friend test', () => {
+    const user = new User('Hello', 'world');
+    const friend = new User('Goodbye', 'world');
+    expect(user.isFriend(friend)).toEqual(false);
+    user.addFriend(friend);
+    expect(user.isFriend(friend)).toEqual(true);
+    expect(friend.isFriend(user)).toEqual(true);
   });
 });
