1b01f813bf297cad2794de3d7c6db80244b839f7
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2022-12-01 21:24:49 +0100

    file for finding the best treshold

---
+++ src/keystroke-fingerprinting/keystroke-test.ts
@@ -0,0 +1,47 @@
+import { emitKeypressEvents } from 'readline';
+import * as readline from 'node:readline/promises';
+import * as timing from '../chat-client/chat-timing.js';
+import * as calculate from '../keystroke-fingerprinting/calculate-delta.js';
+import { writeFile } from 'node:fs';
+
+const result = [];
+const zinnen = [
+  'ik ben een mens',
+  'ik woon in Leuven',
+  'Lukaku had moeten scoren',
+  'zinnen typen is leuk',
+  'Ik vind mezelf leuk',
+  'vind ik mezelf leuk?',
+  'ik denk het wel',
+  'of toch niet?',
+  'achja, we zullen het nooit weten',
+];
+type PromptUserReturntype = {
+  text: string;
+  timings: Array<[string, number]>;
+};
+for (let i = 0; i < 8; i++) {
+  const rll = readline.createInterface({
+    input: process.stdin,
+
+    output: process.stdout,
+  });
+
+  const TEXT = zinnen[i];
+  if (TEXT !== undefined) {
+    const TimingAndText: PromptUserReturntype = await timing.promptUserInput(
+      rll,
+      `Type the following paragraph: \n ${TEXT}`
+    );
+    const Ngram2Time: Map<string, number> = calculate.calculateDelta(TimingAndText.timings, 2);
+    const obj = Object.fromEntries(Ngram2Time);
+    result.push(obj);
+  }
+}
+
+const data = JSON.stringify(result);
+console.log(data);
+writeFile('message.txt', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
