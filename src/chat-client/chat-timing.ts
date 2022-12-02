ffc7fc84c11301f448fbf86c1db140a1c22f66cf
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2022-12-02 15:16:59 +0100

    function with 2 arguments instead of 1

--- src/chat-client/chat-timing.ts
+++ src/chat-client/chat-timing.ts
@@ -1,3 +1,4 @@
+import { cp } from 'node:fs';
 import { emitKeypressEvents } from 'node:readline';
 import type * as readline from 'node:readline/promises';
 // import Debug from 'debug';
@@ -23,16 +24,19 @@ type PromptUserReturntype = {
 export const HELPER = {
   FindTimePress: (): Array<[string, number]> => {
     const timings: Array<[string, number]> = [];
-    const UninformativeKeys: string[] = ['\b', '\r'];
+    const UninformativeKeys: string[] = ['backspace', 'return'];
     process.stdin.on('keypress', function (character: string, key: keyInterface): void {
       let state = true;
       for (const element of UninformativeKeys) {
-        if (key.sequence === element) {
+        if (key.name === element) {
           state = false;
         }
       }
       if (state) {
         timings.push([key.sequence, Date.now()]);
+      }
+      if (key.name === UninformativeKeys[0]) {
+        timings.pop();
       }
       if (key.ctrl && key.name === 'c') process.exit();
     });
@@ -47,13 +51,14 @@ export const HELPER = {
  * @param rll readline.Interface , readline interface where we can ask questions, rll.question()
  * @returns '{text: answer, timings: timing }' where answer is a string, and timing is of type Array<[string, number]>
  */
-export async function promptUserInput(rll: readline.Interface): Promise<PromptUserReturntype> {
+export async function promptUserInput(rll: readline.Interface, question: string): Promise<PromptUserReturntype> {
+  process.stdin.setEncoding('utf8');
   emitKeypressEvents(process.stdin);
   if (process.stdin.isTTY) {
     process.stdin.setRawMode(true);
   }
   const timing: Array<[string, number]> = HELPER.FindTimePress();
-  const answer = await rll.question('>:');
+  const answer = await rll.question(`> ${question}`);
   rll.close();
   return { text: answer, timings: timing };
 }
--- src/keystroke-fingerprinting/keystroke.ts
+++ src/keystroke-fingerprinting/keystroke.ts
@@ -2,6 +2,9 @@ import { emitKeypressEvents } from 'readline';
 import * as readline from 'node:readline/promises';
 import * as timing from '../chat-client/chat-timing.js';
 import * as calculate from '../keystroke-fingerprinting/calculate-delta.js';
+import * as impost from '../keystroke-fingerprinting/imposter.js';
+import { aMeasure, rMeasure } from './measures.js';
+import { CompareTwoMaps } from './compare-two-maps.js';
 
 type PromptUserReturntype = {
   text: string;
@@ -13,9 +16,20 @@ const rll = readline.createInterface({
 
   output: process.stdout,
 });
-const TEXT = 'hammpo';
+const TEXT = 'ik ben noch een kat, noch een huis';
 const TimingAndText: PromptUserReturntype = await timing.promptUserInput(
   rll,
+  `Type the following paragraph: \n ${TEXT}`
+);
+
+const rll1 = readline.createInterface({
+  input: process.stdin,
+
+  output: process.stdout,
+});
+
+const TimingAndText1: PromptUserReturntype = await timing.promptUserInput(
+  rll1,
   `Type the following paragraph: \n ${TEXT}`
 );
 
@@ -24,6 +38,13 @@ const TimingAndText: PromptUserReturntype = await timing.promptUserInput(
 //keystroke-delta functie hieruitvoeren.
 
 const Ngram2Time: Map<string, number> = calculate.calculateDelta(TimingAndText.timings, 2);
+const Ngram2Time2: Map<string, number> = calculate.calculateDelta(TimingAndText1.timings, 2);
 
-console.log(TimingAndText);
 console.log(Ngram2Time);
+console.log(Ngram2Time2);
+// console.log(aMeasure(Ngram2Time, Ngram2Time2));
+// console.log(rMeasure(CompareTwoMaps(Ngram2Time, Ngram2Time2)));
+// console.log(CompareTwoMaps(Ngram2Time, Ngram2Time2));
+// const bool = impost.Detective(Ngram2Time, Ngram2Time2, 0.32, 0.25, 0.75);
+
+// console.log(bool);
