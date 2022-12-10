05f1584b0f39edc1b72b6c41b81ce12054d6fea0
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2022-12-10 07:16:11 +0100

    minor documentation additions for scripts

--- src/scripts/moveserverin.ts
+++ src/scripts/moveserverin.ts
@@ -1,4 +1,11 @@
+//Author: Barteld Van Nieuwenhove
+//Date: 2022/12/10
+
 import fs from 'fs';
+
+/**
+ * Moves the server from the backup folder into the folder where it will be loaded.
+ */
 if (fs.existsSync('./assets/database/backup-server/' + 'server' + '.json')) {
   fs.copyFile(
     './assets/database/backup-server/' + 'server' + '.json',
--- src/scripts/moveserverout.ts
+++ src/scripts/moveserverout.ts
@@ -1,4 +1,12 @@
+//Author: Barteld Van Nieuwenhove
+//Date: 2022/12/10
+
 import fs from 'fs';
+
+/**
+ * Moves the server from its folder into the back-up folder where it will not be loaded.
+ * Used to create space for temporary test servers during npm test runs.
+ */
 if (fs.existsSync('./assets/database/server/' + 'server' + '.json')) {
   fs.copyFile(
     './assets/database/server/' + 'server' + '.json',
