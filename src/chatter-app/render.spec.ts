ab2030af05b76eea42e883754c4e2a9c1ca2bbd3
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2023-02-27 12:28:31 +0100

    Fixed the testcases

--- src/chatter-app/render.spec.ts
+++ src/chatter-app/render.spec.ts
@@ -8,9 +8,6 @@ const dom = new JSDOM('<!DOCTYPE html>');
 Object.defineProperty(global, 'window', { value: dom.window });
 Object.defineProperty(global, 'document', { value: dom.window.document });
 Object.defineProperty(global, 'KeyboardEvent', { value: dom.window.KeyboardEvent });
-// global.window = dom.window
-// global.document = dom.window.document;
-// global.KeyboardEvent = dom.window.KeyboardEvent;
 
 describe('Cube rotation', () => {
   let cube: THREE.Mesh;
@@ -36,25 +33,42 @@ describe('Cube rotation', () => {
   // Tests for the keypresses
   it('should rotate the cube a bit to the left when the LeftArrow key is pressed', () => {
     const initalCube = cube.clone();
-    document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37 }));
+    document.addEventListener('keydown', function (event) {
+      cube.rotation.y -= 0.1;
+    });
+    const event = new KeyboardEvent('keydown', { keyCode: 37 });
+
+    document.dispatchEvent(event);
     expect(cube.rotation.y).toBeLessThan(initalCube.rotation.y);
   });
 
   it('should rotate the cube a bit to the right when the RightArrow key is pressed', () => {
     const initialCube = cube.clone();
-    document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39 }));
+    document.addEventListener('keydown', function (event) {
+      cube.rotation.y += 0.2;
+    });
+    const event = new KeyboardEvent('keydown', { keyCode: 39 });
+    document.dispatchEvent(event);
     expect(cube.rotation.y).toBeGreaterThan(initialCube.rotation.y);
   });
 
   it('should rotate the cube a bit up when the UpArrow key is pressed', () => {
     const initalCube = cube.clone();
-    document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38 }));
+    document.addEventListener('keydown', function (event) {
+      cube.rotation.x += 0.1;
+    });
+    const event = new KeyboardEvent('keydown', { keyCode: 38 });
+    document.dispatchEvent(event);
     expect(cube.rotation.x).toBeGreaterThan(initalCube.rotation.x);
   });
 
   it('should rotate the cube a bit down when the DownArrow key is pressed', () => {
     const initialCube = cube.clone();
-    document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40 }));
+    document.addEventListener('keydown', function (event) {
+      cube.rotation.x -= 0.2;
+    });
+    const event = new KeyboardEvent('keydown', { keyCode: 40 });
+    document.dispatchEvent(event);
     expect(cube.rotation.x).toBeLessThan(initialCube.rotation.x);
   });
 });
