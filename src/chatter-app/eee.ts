7a2235a5144c4e192c96dbb24c8ee7e38ae874ff
Maité Desmedt <maite.desmedt@student.kuleuven.be>
2023-03-07 19:02:02 +0100

    cleaner code+ boring colors

--- src/chatter-app/eee.ts
+++ src/chatter-app/eee.ts
@@ -10,6 +10,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let INTERSECTED: THREE.Object3D<THREE.Event> | null = null;
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xb6d2e0);
 const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
 //camera.position.set(-17, 31, 33);
 camera.position.z = 20;
@@ -38,61 +39,48 @@ const hlm200 = 0.5;
 const hs200 = 0.7;
 const ha200 = 1.1;
 
+// construction of the shape and spatial planning of the objects that are part of the buildings
 const geoGround = new THREE.PlaneGeometry(25, 20);
 geoGround.rotateX(THREE.MathUtils.degToRad(-90));
 const geoCentralCube = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-const geok200 = new THREE.BoxGeometry(1.3, hk200, 1.9);
-geok200.translate(-9.85, heightsaver + hk200 * 0.5, -0.65);
-const geoAcco = new THREE.BoxGeometry(0.8, hacco, 3.3);
-geoAcco.translate(-11, heightsaver + hacco * 0.5, 0.25);
-const geol200 = new THREE.BoxGeometry(1.3, hlm200, 2.0);
-geol200.translate(-8.15, heightsaver + hlm200 * 0.5, -3.3);
-const geos200 = new THREE.BoxGeometry(0.6, hs200, 1.3);
-geos200.translate(-7.2, heightsaver + hs200 * 0.5, -3.65);
-const geom200 = new THREE.BoxGeometry(1.8, hlm200, 1.9);
-geom200.translate(-6, heightsaver + hlm200 * 0.5, -3.75);
-const geoa200 = new THREE.BoxGeometry(0, 0, 0);
-makeBuilding('a200', ha200, 3.6, 0.9, -7.2, 1.4);
-makeBuilding('a200', ha200, 0.7, 0.1, -7.15, 0.95);
-makeBuilding('a200', ha200, 0.7, 0.1, -7.15, 1.9);
 
 const centralCube = new THREE.Mesh(geoCentralCube, new THREE.MeshStandardMaterial({ color: 0x0000ff }));
 scene.add(centralCube);
 const ground = new THREE.Mesh(geoGround, new THREE.MeshStandardMaterial({ color: 0x00ff00 }));
 ground.receiveShadow = true;
 scene.add(ground);
-const k200 = new THREE.Mesh(geok200, new THREE.MeshStandardMaterial({ color: 0x0000ff }));
-scene.add(k200);
-k200.castShadow = true;
-k200.layers.set(1);
-const acco = new THREE.Mesh(geoAcco, new THREE.MeshStandardMaterial({ color: 0x000020 }));
-scene.add(acco);
-acco.castShadow = true;
-acco.layers.set(1);
-const s200 = new THREE.Mesh(geos200, new THREE.MeshStandardMaterial({ color: 0xdd0022 }));
-scene.add(s200);
-s200.castShadow = true;
-s200.layers.set(1);
-const m200 = new THREE.Mesh(geom200, new THREE.MeshStandardMaterial({ color: 0xff0000 }));
-scene.add(m200);
-m200.castShadow = true;
-m200.layers.set(1);
-const l200 = new THREE.Mesh(geol200, new THREE.MeshStandardMaterial({ color: 0xff00ff }));
-scene.add(l200);
-l200.castShadow = true;
-l200.layers.set(1);
-const a200 = new THREE.Mesh(geoa200, new THREE.MeshStandardMaterial({ color: 0xfcba03 }));
-scene.add(a200);
-a200.castShadow = true;
-a200.layers.set(1);
+
+// construction of the buildings + finishing touches (= adding objects to scene + assign layer + castShadow) 
+const k200 = new THREE.Mesh(makeGeo(1.3, hk200, 1.9, -9.85, -0.65), makeMaterial(0x3b5263));
+finishingTouches(k200, 1, true);
+const acco = new THREE.Mesh(makeGeo(0.8, hacco, 3.3, -11, 0.25), makeMaterial(0x3b5263));
+finishingTouches(acco, 1, true);
+const s200 = new THREE.Mesh(makeGeo(0.6, hs200, 1.3, -7.2, -3.65), makeMaterial(0x3b5263));
+finishingTouches(s200, 1, true);
+const m200 = new THREE.Mesh(makeGeo(1.8, hlm200, 1.9, -6, -3.75), makeMaterial(0x758694 ));
+finishingTouches(m200, 1, true);
+const l200 = new THREE.Mesh(makeGeo(1.3, hlm200, 2.0, -8.15, -3.3), makeMaterial(0x758694 ));
+finishingTouches(l200, 1, true);
+const a200big = new THREE.Mesh(makeGeo(3.6, ha200, 0.9, -7.2, 1.4), makeMaterial(0xa9aaab));
+const a200n = new THREE.Mesh(makeGeo(0.7, ha200, 0.1, -7.15, 0.95), makeMaterial(0xf52f07));
+const a200s = new THREE.Mesh(makeGeo(0.7, ha200, 0.1, -7.15, 1.9), makeMaterial(0xf52f07));
+const a200Group = new THREE.Group();
+a200Group.add(a200big);
+a200Group.add(a200n);
+a200Group.add(a200s);
+finishingTouches(a200Group, 1, true);
+
+// labels for the buildings 
 const m200Div = document.createElement('div');
 m200Div.className = 'label';
-m200Div.textContent = 'm200';
+m200Div.textContent = '200M';
 m200Div.style.marginTop = '-lem';
+m200Div.style.color = "black" ;
 const m200Label = new CSS2DObject(m200Div);
-m200Label.position.set(1.8, hlm200, 1.9);
+m200Label.position.set(-6, hlm200*1.5, -3.75);
 m200.add(m200Label);
 
+// light and shadow
 const directionalLight = new THREE.PointLight(0xffffff, 0.5, 100);
 directionalLight.castShadow = true;
 directionalLight.position.set(0, 10, 4);
@@ -102,6 +90,7 @@ scene.add(directionalLight);
 const light = new THREE.AmbientLight(0x404040, 0.8); // soft white light
 scene.add(light);
 
+//enables user to move the camera when dragging the mouse:
 // Create a mouse vector to store the mouse position.
 const mouse = new THREE.Vector2();
 function onDocumentMouseMove(event: { clientX: number; clientY: number }) {
@@ -116,22 +105,25 @@ controls.target.set(0, 0, 0);
 controls.dampingFactor = 0.05;
 controls.enableDamping = true;
 
-function makeBuilding(
-  building: string,
-  height: number,
-  xlength: number,
-  zlength: number,
-  xtranslate: number,
-  ztranslate: number
-) {
+
+function makeGeo(xlength:number, height: number, zlength:number, xpos:number, zpos:number){
   const geo = new THREE.BoxGeometry(xlength, height, zlength);
-  geo.translate(xtranslate, heightsaver + height * 0.5, ztranslate);
-  if (building === 'a200') {
-    // geoa200 = mergeBufferGeometries([geo, geoa200]);
-  }
+  geo.translate(xpos, heightsaver + height * 0.5, zpos);
+  return geo;
+}
+
+function makeMaterial(mycolor:number){
+  return new THREE.MeshStandardMaterial({ color: mycolor })
+}
+
+function finishingTouches(building: THREE.Mesh | THREE.group, layer:number, castShadowB:Boolean){
+  building.layers.set(layer);
+  building.castShadow = castShadowB;
+  scene.add(building);
 }
 
 function animate() {
+  // hovering
   const raycaster = new THREE.Raycaster();
   raycaster.layers.set(1);
   raycaster.setFromCamera(mouse, camera);
