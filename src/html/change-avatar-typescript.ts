787aad7bf425361b8e7368516b4a71cecaf5ee3b
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2023-03-21 15:11:08 +0100

    typescript files

---
+++ src/html/change-avatar-typescript.ts
@@ -0,0 +1,5 @@
+// function changeAvatar(avatarName: string): void {
+//   console.log(avatarName);
+//   const profileImage = document.getElementById('profile-image') as HTMLImageElement;
+//   profileImage.src = `img/avatars/${avatarName}`;
+// }
