a20102743210154ae2e84d389c738312439dddb7
John Gao <john.gao@student.kuleuven.be>
2023-03-21 18:32:43 +0100

    finished with documentation and unit tests

--- src/meldingen/meldingen.spec.ts
+++ src/meldingen/meldingen.spec.ts
@@ -1,19 +1,43 @@
-import { expect, it, describe } from 'vitest';
+import { expect, it, describe, vi } from 'vitest';
 import { JSDOM } from 'jsdom';
-import { showNotification } from './meldingen.js';
+import { closeNotification, showNotification } from './meldingen.js';
 
 describe('showNotification', () => {
   const dom = new JSDOM(
     '<!DOCTYPE html><div class="toast" id="myToast"></div><div class="toast-body" id="toastbody"></div>'
   );
   const document = dom.window.document;
+  const window = dom.window;
   const name = 'testuser';
   const element = document.getElementById('myToast') as HTMLElement;
   const body = document.getElementById('toastbody') as HTMLElement;
 
-  /* it('changes the class of the toast to show it', () => {
-    showNotification(name);
-    console.log(body.innerText);
+  it('changes the class of the toast to show it, hides it after 6 seconds', () => {
+    vi.useFakeTimers();
+    showNotification(document, window, name);
     expect(element.classList.contains('show')).toBe(true);
-  }); */
+    expect(body.innerText).toContain(name);
+    vi.runAllTimers();
+    expect(element.classList.contains('show')).toBe(false);
+    vi.useRealTimers();
+  });
+});
+
+describe('closeNotification', () => {
+  const dom = new JSDOM(
+    '<!DOCTYPE html><div class="toast" id="myToast"></div><div class="toast-body" id="toastbody"></div>'
+  );
+  const document = dom.window.document;
+  const window = dom.window;
+  const name = 'testuser';
+  const element = document.getElementById('myToast') as HTMLElement;
+
+  it('clears timeout of toast and hides it', () => {
+    const spyOnClearTimeOut = vi.spyOn(global, 'clearTimeout');
+    const id = showNotification(document, window, name);
+    expect(element.classList.contains('show')).toBe(true);
+    closeNotification(document, window, id);
+    expect(element.classList.contains('show')).toBe(false);
+    expect(spyOnClearTimeOut).toHaveBeenCalledOnce();
+  });
 });
--- src/meldingen/meldingen.ts
+++ src/meldingen/meldingen.ts
@@ -1,25 +1,45 @@
-let element_id: number;
+// @Author John Gao
+// @date-start 07/03/2023, @date-updated 21/03/2023
 
-export function showNotification(sender: string) {
-  if (typeof window === 'object') {
-    document.addEventListener('DOMContentLoaded', function () {
-      const element = document.getElementById('myToast') as HTMLElement;
-      const body = document.getElementById('toastbody') as HTMLElement;
-      if (!element.classList.contains('show')) {
-        element.classList.add('show');
-        const name = sender;
-        const message = 'You just received a message from ' + name;
-        body.innerText = message;
-        element_id = window.setTimeout(function () {
-          element.classList.remove('show');
-        }, 6000);
-      }
-    });
+import type { DOMWindow } from 'jsdom';
+
+/**
+ * This function will show the user a notification, using a toast (bootstrap).
+ * When called, a notification pops up in the top right corner, and disappears automatically after 6 seconds using setTimeout.
+ * @param document Document, HTML file containing the toast
+ * @param window Window, the opened window in the browser
+ * @param sender string, the person sending the message to the user
+ * @returns number, the timeout id of the notification
+ */
+export function showNotification(document: Document, window: Window | DOMWindow, sender: string): number {
+  if (typeof window !== 'undefined') {
+    const element = document.getElementById('myToast') as HTMLElement;
+    const body = document.getElementById('toastbody') as HTMLElement;
+    if (!element.classList.contains('show')) {
+      element.classList.add('show');
+      const name = sender;
+      const message = 'You just received a message from ' + name;
+      body.innerText = message;
+      const element_id = window.setTimeout(function () {
+        element.classList.remove('show');
+      }, 6000);
+      return element_id;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
   }
 }
 
-export function closeNotification() {
-  if (typeof window === 'object') {
+/**
+ * This function will close the toast notification and clear its timeout value using the timeout id.
+ * @param document Document, HTML file containing the toast
+ * @param window Window, the opened window in the browser
+ * @param element_id number, the timeout id of the notification to be closed
+ */
+export function closeNotification(document: Document, window: Window | DOMWindow, element_id: number) {
+  if (typeof window !== 'undefined') {
     const element = document.getElementById('myToast') as HTMLElement;
     if (element_id) clearTimeout(element_id);
     element.classList.remove('show');
