51f7dd5c79da065fabdc75fd8acd1d19910c81d1
Thomas Evenepoel <thomas.evenepoel@student.kuleuven.be>
2023-03-28 15:05:40 +0200

    delete javascript file

--- src/chatter-app/html/change-avatar-typescripte.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable no-undef */
-function changeAvatar(avatarName) {
-  console.log(avatarName);
-  var profileImage = document.getElementById('profile-image');
-}
