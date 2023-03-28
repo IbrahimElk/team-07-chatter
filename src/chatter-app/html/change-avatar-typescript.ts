b9e61fa72c37a3eed16405f9545523d222bb0b65
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2023-03-28 18:49:37 +0200

    merge confliiects

--- src/chatter-app/html/change-avatar-typescript.ts
+++ src/chatter-app/html/change-avatar-typescript.ts
@@ -1,5 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+document.body.addEventListener('click', addEventListeners, false);
+
+function addEventListeners(event: Event) {
+  event = event || window.event;
+  if (!(event.target instanceof HTMLElement)) return;
+  const target = event.target;
+  let element = target;
+
+  while (element) {
+    console.log('hey');
+    if (element.nodeName === 'BUTTON' && /dropdown-item/.test(element.className)) {
+      changeAvatar(element.id);
+      break;
+    }
+    const test = element.parentElement;
+    if (test !== null) {
+      element = test;
+    }
+  }
+}
 function changeAvatar(avatarName: string): void {
   console.log(avatarName);
   const profileImage = document.getElementById('profile-image') as HTMLImageElement;
-  profileImage.src = `img/avatars/${avatarName}`;
+  profileImage.src = `img/avatars/${avatarName}` + '.png';
 }
