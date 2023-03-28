9d5764f66970b3157d6a1a12811e781bc489c19d
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2023-03-28 18:54:13 +0200

    joo

---
+++ src/chatter-app/html/change-avatar-typescripte.js
@@ -0,0 +1,5 @@
+// function changeAvatar(avatarName) {
+//   console.log(avatarName);
+//   var profileImage = document.getElementById('profile-image');
+//   profileImage.src = 'img/avatars/'.concat(avatarName);
+// }
--- src/client/html/change-avatar-typescript.ts
+++
@@ -1,5 +0,0 @@
-function changeAvatar(avatarName: string): void {
-  console.log(avatarName);
-  const profileImage = document.getElementById('profile-image') as HTMLImageElement;
-  profileImage.src = `img/avatars/${avatarName}`;
-}
