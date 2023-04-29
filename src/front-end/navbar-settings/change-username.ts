3c48fce0292d48a7b85fb81ca9286a5eb440bd3a
Amélie Van Loock <amelie.vanloock@student.kuleuven.be>
2023-04-29 20:05:25 +0200

    unit test: document not defined

--- src/front-end/navbar-settings/change-username.ts
+++ src/front-end/navbar-settings/change-username.ts
@@ -3,15 +3,7 @@ import type { IWebSocket } from '../proto/ws-interface.js';
 import type * as ClientInterfaceTypes from '../proto/client-types.js';
 import type * as ServerInterfaceTypes from '../proto/server-types.js';
 
-declare var bootstrap: any;
-const myModal = new bootstrap.Modal(document.querySelector('#exampleModal'));
 
-
-export function Close() {
-  const closeButton = document.querySelector('.btn btn-secondary');
-  myModal.hide()
-  
-}
 
 const verzendButton = document.getElementById('verzend-button') as HTMLButtonElement;
 verzendButton.addEventListener('click', () => {
@@ -19,7 +11,12 @@ verzendButton.addEventListener('click', () => {
   changeUsername(ClientUser.getWebSocket()) 
 });
 
-function changeUsername(ws: IWebSocket) {
+/**
+   * Change the username.
+   *
+   * @param ws websocket, a websocket that is connected to the server.
+   */
+export function changeUsername(ws: IWebSocket) {
     const username= (document.getElementById("usernameInput") as HTMLInputElement).value;
   
     const changeusername: ClientInterfaceTypes.changeUsername = {
