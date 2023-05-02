f8c312660ee33ebf66a64809734d012f41e5c30b
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2023-05-02 16:56:16 +0200

    connect-channel spec

--- src/server-dispatcher/server-channel-logic/connect-channel.spec.ts
+++ src/server-dispatcher/server-channel-logic/connect-channel.spec.ts
@@ -1,82 +1,94 @@
-// import { ChatServer } from '../../server/chat-server.js';
-// import { User } from '../../objects/user/user.js';
-// import { DirectMessageChannel } from '../../objects/channel/directmessagechannel.js';
-// import { MockWebSocket, MockWebSocketServer } from '../../protocol/__mock__/ws-mock.js';
+import { ChatServer } from '../../server/chat-server.js';
+import { User } from '../../objects/user/user.js';
+import { DirectMessageChannel } from '../../objects/channel/directmessagechannel.js';
+import { MockWebSocket, MockWebSocketServer } from '../../front-end/proto/__mock__/ws-mock.js';
+import type * as ServerInterfaceTypes from '../../front-end/proto/server-types.js';
+import type * as ClientInterfaceTypes from '../../front-end/proto/client-types.js';
 import { describe, expect, it, vi } from 'vitest';
-// import { selectChannel } from './select-channel.js';
-// import { PublicChannel } from '../../objects/channel/publicchannel.js';
+import { connectChannel } from './connect-channel.js';
+import { PublicChannel } from '../../objects/channel/publicchannel.js';
 
-describe('selectChannel', () => {
-  //   const wss = new MockWebSocketServer('URLS');
-  //   const chatServer = new ChatServer(wss, new Set<string>(), new Set<string>());
-  //   const ws = new MockWebSocket('URL');
-  //   const me = new User('me', 'my_password', '@me');
-  //   const friend = new User('friend', 'friend_password', '@friend');
-  //   const channel = new PublicChannel('channelName', '#lescode');
-  //   // me.addChannel(channel.getChannelId());
-  //   // me.addChannel(directChannel.getChannelId());
-  //   // friend.addChannel(directChannel.getChannelId());
+describe('connectChannem', () => {
+  const wss = new MockWebSocketServer('URLS');
+  const chatServer = new ChatServer(wss, new Set<string>(), new Set<string>());
+  const ws = new MockWebSocket('URL');
+  const me = new User('me', 'my_password');
+  me.setSessionID('meID');
+  const friend = new User('friend', 'friend_password');
+  friend.setSessionID('friendID');
+  const channel = new PublicChannel('channelName');
+  const directChannel = new DirectMessageChannel(me, friend);
+  me.addFriend(friend, channel);
+  me.addPublicChannel(directChannel.getCUID());
 
   it('should return userNotConnected error if the user is not connected', async () => {
-    //     const spygetUserByWebsocket = vi
-    //       .spyOn(chatServer, 'getUserByWebsocket')
-    //       .mockReturnValue(Promise.resolve(undefined));
-    //     const spy = vi.spyOn(ws, 'send');
-    //     await selectChannel({ channelCuid: channel.getCUID() }, chatServer, ws);
-    //     expect(spy).toHaveBeenCalledWith(
-    //       JSON.stringify({
-    //         command: 'selectChannelSendback',
-    //         payload: { succeeded: false, typeOfFail: 'userNotConnected' },
-    //       })
-    //     );
+    const spygetUserByWebsocket = vi
+      .spyOn(chatServer, 'getUserByWebsocket')
+      .mockReturnValue(Promise.resolve(undefined));
+    const spy = vi.spyOn(ws, 'send');
+    await connectChannel({ sessionID: 'fakeSessionID', channelCUID: channel.getCUID() }, chatServer, ws);
+    expect(spy).toHaveBeenCalledWith(
+      JSON.stringify({
+        command: 'connectChannelSendback',
+        payload: { succeeded: false, typeOfFail: 'userNotConnected' },
+      })
+    );
   });
 
-  //   it('should return channelNotExisting error if the channel does not exist', async () => {
-  //     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValue(Promise.resolve(me));
-  //     const spygetChannelById = vi
-  //       .spyOn(chatServer, 'getPublicChannelByChannelId')
-  //       .mockReturnValue(Promise.resolve(undefined));
-  //     const spy = vi.spyOn(ws, 'send');
+  it('should return channelNotExisting error if the channel does not exist', async () => {
+    const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValue(Promise.resolve(me));
+    const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValue(Promise.resolve(undefined));
+    const spy = vi.spyOn(ws, 'send');
+    chatServer.cachUser(me);
+    const notExistingChannelId = '789';
+    await connectChannel({ sessionID: 'meID', channelCUID: notExistingChannelId }, chatServer, ws);
+    expect(spy).toHaveBeenCalledWith(
+      JSON.stringify({
+        command: 'connectChannelSendback',
+        payload: { succeeded: false, typeOfFail: 'channelNotExisting' },
+      })
+    );
+  });
 
-  //     const notExistingChannelId = '789';
-  //     await selectChannel({ channelCuid: notExistingChannelId }, chatServer, ws);
-  //     expect(spy).toHaveBeenCalledWith(
-  //       JSON.stringify({
-  //         command: 'selectChannelSendback',
-  //         payload: { succeeded: false, typeOfFail: 'channelNotExisting' },
-  //       })
-  //     );
-  //   });
+  it('should return userNotAllowedToConnect error if the user is not allowed to enter', async () => {
+    const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValue(Promise.resolve(me));
+    const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValue(Promise.resolve(channel));
+    vi.spyOn(channel, 'isAllowedToConnect').mockReturnValue(false);
+    const spy = vi.spyOn(ws, 'send');
+    chatServer.cachUser(me);
+    chatServer.setCachePublicChannel(channel);
+    await connectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
+    expect(spy).toHaveBeenCalledWith(
+      JSON.stringify({
+        command: 'connectChannelSendback',
+        payload: { succeeded: false, typeOfFail: 'userNotAllowedToConnect' },
+      })
+    );
+  });
 
-  //   it('should return userNotMemberOfChannel error if the user is not a member of the channel', async () => {
-  //     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValue(Promise.resolve(me));
-  //     const spygetChannelById = vi
-  //       .spyOn(chatServer, 'getPublicChannelByChannelId')
-  //       .mockReturnValue(Promise.resolve(channel));
-  //     const spy = vi.spyOn(ws, 'send');
+  it('should return the channel if it exists', async () => {
+    const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValueOnce(Promise.resolve(me));
+    const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValueOnce(Promise.resolve(channel));
+    vi.spyOn(channel, 'isAllowedToConnect').mockReturnValueOnce(true);
+    const spy = vi.spyOn(ws, 'send');
+    chatServer.cachUser(me);
+    chatServer.setCachePublicChannel(channel);
+    await connectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
+    const msgback: ServerInterfaceTypes.channelInfo['payload'] = {
+      connections: new Array<ClientInterfaceTypes.PublicUser>(),
+      messages: new Array<{
+        user: ClientInterfaceTypes.PublicUser;
+        text: string;
+        date: string;
+        trust: number;
+      }>(),
+    };
 
-  //     await selectChannel({ channelCuid: channel.getCUID() }, chatServer, ws);
-  //     expect(spy).toHaveBeenCalledWith(
-  //       JSON.stringify({
-  //         command: 'selectChannelSendback',
-  //         payload: { succeeded: false, typeOfFail: 'userNotMemberOfChannel' },
-  //       })
-  //     );
-  //   });
-
-  //   it('should return the channel if it exists', async () => {
-  //     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserByWebsocket').mockReturnValue(Promise.resolve(me));
-  //     const spygetChannelById = vi
-  //       .spyOn(chatServer, 'getPublicChannelByChannelId')
-  //       .mockReturnValue(Promise.resolve(channel));
-  //     const spy = vi.spyOn(ws, 'send');
-  //     channel.addUser(me.getUUID());
-  //     await selectChannel({ channelCuid: channel.getCUID() }, chatServer, ws);
-  //     expect(spy).toHaveBeenCalledWith(
-  //       JSON.stringify({
-  //         command: 'selectChannelSendback',
-  //         payload: { messages: [], succeeded: true },
-  //       })
-  //     );
-  //   });
+    expect(spy).toHaveBeenCalledWith(
+      JSON.stringify({
+        command: 'channelInfo',
+        payload: msgback,
+      })
+    );
+  });
 });
