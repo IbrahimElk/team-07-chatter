9fb296e901dc9504862af34024e3be7ebfe9914e
Barteld Van Nieuwenhove <barteld.vannieuwenhove@student.kuleuven.be>
2023-05-10 18:24:12 +0200

    remove redundant test, fix disconnect test, fix connectTest

--- test/server-dispatcher/server-channel-logic/disconnect-channel.spec.ts
+++ test/server-dispatcher/server-channel-logic/disconnect-channel.spec.ts
@@ -5,14 +5,16 @@ import { MockWebSocket, MockWebSocketServer } from '../../../src/front-end/proto
 import type * as ServerInterfaceTypes from '../../../src/front-end/proto/server-types.js';
 import type * as ClientInterfaceTypes from '../../../src/front-end/proto/client-types.js';
 import { describe, expect, it, vi } from 'vitest';
-import { connectChannel } from '../../../src/server-dispatcher/server-channel-logic/connect-channel.js';
 import { PublicChannel } from '../../../src/objects/channel/publicchannel.js';
+import { disconnectChannel } from '../../../src/server-dispatcher/server-channel-logic/disconnect-channel.js';
+import { IWebSocket } from '../../../src/front-end/proto/ws-interface.js';
 
-describe('connectChannel', () => {
+describe('disconnectChannel', () => {
   const wss = new MockWebSocketServer('URLS');
   const chatServer = new ChatServer(wss, new Set<string>(), new Set<string>());
-  const ws = new MockWebSocket('URL');
+  const ws = new MockWebSocket('URL', 'client1');
   const me = new User('me', 'my_password');
+  me.setWebsocket(ws);
   me.setSessionID('meID');
   const friend = new User('friend', 'friend_password');
   friend.setSessionID('friendID');
@@ -26,10 +28,10 @@ describe('connectChannel', () => {
       .spyOn(chatServer, 'getUserBySessionID')
       .mockReturnValue(Promise.resolve(undefined));
     const spy = vi.spyOn(ws, 'send');
-    await connectChannel({ sessionID: 'fakeSessionID', channelCUID: channel.getCUID() }, chatServer, ws);
+    await disconnectChannel({ sessionID: 'fakeSessionID', channelCUID: channel.getCUID() }, chatServer, ws);
     expect(spy).toHaveBeenCalledWith(
       JSON.stringify({
-        command: 'connectChannelSendback',
+        command: 'disconnectChannelSendback',
         payload: { succeeded: false, typeOfFail: 'userNotConnected' },
       })
     );
@@ -39,49 +41,52 @@ describe('connectChannel', () => {
     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserBySessionID').mockReturnValue(Promise.resolve(me));
     const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValue(Promise.resolve(undefined));
     const spy = vi.spyOn(ws, 'send');
-    await connectChannel({ sessionID: 'meID', channelCUID: '789' }, chatServer, ws);
+    // chatServer.cacheUser(me);
+    await disconnectChannel({ sessionID: 'meID', channelCUID: '789' }, chatServer, ws);
     expect(spy).toHaveBeenCalledWith(
       JSON.stringify({
-        command: 'connectChannelSendback',
+        command: 'disconnectChannelSendback',
         payload: { succeeded: false, typeOfFail: 'channelNotExisting' },
       })
     );
   });
 
-  it('should return userNotAllowedToConnect error if the user is not allowed to enter', async () => {
+  it('should return userNotAllowedToConnect error if the user is not connected to channel', async () => {
     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserBySessionID').mockReturnValue(Promise.resolve(me));
     const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValue(Promise.resolve(channel));
-    vi.spyOn(channel, 'isAllowedToConnect').mockReturnValue(false);
+    vi.spyOn(channel, 'isConnectedUser').mockReturnValue(false);
     const spy = vi.spyOn(ws, 'send');
-    await connectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
+    await disconnectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
     expect(spy).toHaveBeenCalledWith(
       JSON.stringify({
-        command: 'connectChannelSendback',
-        payload: { succeeded: false, typeOfFail: 'userNotAllowedToConnect' },
+        command: 'disconnectChannelSendback',
+        payload: { succeeded: false, typeOfFail: 'userNotConnectedToChannel' },
       })
     );
   });
 
-  it('should return the channel if it exists', async () => {
+  it('should return nothing if user is still connected (on other websockets)', async () => {
     const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserBySessionID').mockReturnValueOnce(Promise.resolve(me));
     const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValueOnce(Promise.resolve(channel));
-    vi.spyOn(channel, 'isAllowedToConnect').mockReturnValueOnce(true);
+    vi.spyOn(channel, 'isConnectedUser').mockReturnValueOnce(true);
+    vi.spyOn(me, 'isConnectedToChannel').mockReturnValueOnce(true);
     const spy = vi.spyOn(ws, 'send');
-    await connectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
-    const msgback: ServerInterfaceTypes.channelInfo['payload'] = {
-      connections: new Array<ClientInterfaceTypes.PublicUser>(),
-      messages: new Array<{
-        user: ClientInterfaceTypes.PublicUser;
-        text: string;
-        date: string;
-        trust: number;
-      }>(),
-    };
-
+    await disconnectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
+  });
+  it('should return success if the user is no longer connected (on other websockets)', async () => {
+    const spygetUserByWebsocket = vi.spyOn(chatServer, 'getUserBySessionID').mockReturnValueOnce(Promise.resolve(me));
+    const spygetChannelById = vi.spyOn(chatServer, 'getChannelByCUID').mockReturnValueOnce(Promise.resolve(channel));
+    vi.spyOn(channel, 'isConnectedUser').mockReturnValueOnce(true);
+    vi.spyOn(me, 'isConnectedToChannel').mockReturnValueOnce(false);
+    vi.spyOn(channel, 'getConnectedUsers').mockReturnValueOnce(new Set(me.getUUID()));
+    vi.spyOn(chatServer, 'getUserByUUID').mockReturnValueOnce(Promise.resolve(me));
+    vi.spyOn(me, 'getChannelWebSockets').mockReturnValueOnce(me.getWebSocket());
+    const spy = vi.spyOn(ws, 'send');
+    await disconnectChannel({ sessionID: 'meID', channelCUID: channel.getCUID() }, chatServer, ws);
     expect(spy).toHaveBeenCalledWith(
       JSON.stringify({
-        command: 'channelInfo',
-        payload: msgback,
+        command: 'disconnectChannelSendback',
+        payload: { succeeded: true, user: me.getPublicUser() },
       })
     );
   });
